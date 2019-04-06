6f643801884b149c092ff5e354d7aef8b2534d6b
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2019-04-06 14:04:24 +0300

    Сorrections with comments.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,21 +8,21 @@ public class ArrayStorage {
     int noteNullArraySize = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, noteNullArraySize, null);
         noteNullArraySize = 0;
     }
 
     void save(Resume r) {
-        if (noteNullArraySize <= storage.length - 1) {
+        if (noteNullArraySize < storage.length) {
             storage[noteNullArraySize] = r;
             noteNullArraySize++;
         }
     }
 
     Resume get(String uuid) {
-        for (Resume aStorage : storage) {
-            if (aStorage != null && aStorage.uuid.equals(uuid)) {
-                return aStorage;
+        for (int i = 0; i < noteNullArraySize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
@@ -30,13 +30,12 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int j;               //счетчик цикла
-        for (j = 0; j < storage.length; j++) {
-            if (storage[j] != null && storage[j].uuid.equals(uuid)) {
-                storage[j] = null;
+        for (j = 0; j < noteNullArraySize; j++) {
+            if (storage[j].uuid.equals(uuid)) {
                 break;
             }
         }
-        System.arraycopy(storage, j + 1, storage, j, storage.length - 1 - j);
+        System.arraycopy(storage, j + 1, storage, j, noteNullArraySize - 1 - j);
         noteNullArraySize--;
     }
 
