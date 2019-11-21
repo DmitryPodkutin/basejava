96f68ebb013d5b0e09d411bcb1df1a30d63de259
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2019-11-21 15:38:10 +0300

    Lessons  6  added Comparator in SortedArraysStorage

--- src/ru/javawebinar/basejava/storage/MapStorage.java
+++ src/ru/javawebinar/basejava/storage/MapStorage.java
@@ -44,8 +44,7 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public Resume[] getAll() {
-        return mapStorage.values().toArray(new Resume[0]);
-
+        return mapStorage.values().toArray(new Resume[mapStorage.size()]);
     }
 
     @Override
