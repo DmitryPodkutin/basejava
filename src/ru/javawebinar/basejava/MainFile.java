ebb2605f4c07da371730e621768886371e119a81
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2019-12-14 11:16:48 +0300

    Lesson 8 - MainFile.java (file check notNull)

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,20 +1,22 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
+import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
-     MainFile.recursivePrintFileNames(new File("/Users/Dmitry/Documents/JavaLessons/basejava"));
+        MainFile.recursivePrintFileNames(new File("/Users/Dmitry/Documents/JavaLessons/basejava"));
     }
+
     public static void recursivePrintFileNames(File dir) {
         File[] files = dir.listFiles();
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    recursivePrintFileNames(file);
-                } else if (file.isFile()) {
-                    System.out.println("File name: " + file.getName());
-                }
+        for (File file : Objects.requireNonNull(files)) {
+            if (file.isDirectory()) {
+                recursivePrintFileNames(file);
+            } else if (file.isFile()) {
+                System.out.println("File name: " + file.getName());
             }
         }
     }
+}
 
