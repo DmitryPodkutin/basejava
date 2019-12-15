19042e6795be8b59d83fbec21f6a19397753298c
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2019-12-15 14:09:10 +0300

    Lesson 8 - created method dateFormat(LocalDate date) with  DateUtil.java class.

--- src/util/DateUtil.java
+++ src/util/DateUtil.java
@@ -2,10 +2,16 @@ package util;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
+    }
+
+    public static String dateFormat(LocalDate date) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/YYYY");
+        return dateTimeFormatter.format(date);
     }
 
 }
