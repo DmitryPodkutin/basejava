298825783b6e258315c9ed2497bb36518e024d04
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2020-02-08 11:38:53 +0300

    Lesson12  home work (Streams) - final fix .

--- src/ru/javawebinar/basejava/StreamTest.java
+++ src/ru/javawebinar/basejava/StreamTest.java
@@ -2,27 +2,30 @@ package ru.javawebinar.basejava;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+
 import java.util.stream.Collectors;
 
 public class StreamTest {
     public static void main(String[] args) {
         int[] values = {1, 2, 3, 2, 5};
-        List<Integer> integers = Arrays.asList(1, 2, 7, 9, 4, 6);
+        List<Integer> integers = Arrays.asList(1, 2, 8, 9, 7, 6);
         System.out.println(mainValue(values));
         System.out.println(oddOrEven(integers));
     }
+
     public static int mainValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> 10 * a + b);
+        return Arrays.stream(values)
+                .distinct().sorted()
+                .reduce(0, (a, b) -> 10 * a + b);
     }
+
     public static List<Integer> oddOrEven(List<Integer> integers) {
-        return (integers.stream()
-                .reduce(0, Integer::sum) % 2 == 0
-                ? filter(integers, p -> p % 2 == 0)
-                : filter(integers, p -> p % 2 != 0));
+        int evenOdd = integers.stream()
+                .reduce(0, Integer::sum) % 2;
+        return integers.stream()
+                .filter(p -> p % 2 != evenOdd)
+                .collect(Collectors.toList());
     }
-    private static List <Integer> filter(List <Integer> integers, Predicate<Integer> predicate) {
-        return integers.stream().filter(predicate).collect(Collectors.toList());
-    }
+
 }
 
